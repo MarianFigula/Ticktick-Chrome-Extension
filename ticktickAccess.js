36bdce1a899326e703bd2cc3530be96d306cf896
Marian Figula <majofigulam3@gmail.com>
2023-11-11 12:30:11 +0100

    need to be fixed

--- ticktickAccess.js
+++ ticktickAccess.js
@@ -1,20 +1,30 @@
 
-console.log("A")
-chrome.storage.local.get('authorizationCode', function(data) {
-    console.log("AAA")
-    console.log("authcod: " + data.authorizationCode)
-    if (!data.authorizationCode) {
+// console.log("A")
+// chrome.storage.local.get('authorizationCode', function(data) {
+//     console.log("AAA")
+//     console.log("authcod: " + data.authorizationCode)
+//     if (data.authorizationCode) {
+//
+//         // Define the URL and parameters
+//         const authorizationUrl = `https://ticktick.com/oauth/authorize?` +
+//             `client_id=98tJj0kwfv0IvVmSFb` +
+//             `&scope=tasks:write tasks:read` +
+//             `&state=https://ticktick.com/oauth/authorize` +
+//             `&redirect_uri=https://rezervacie.elfsport.sk/moje-rezervacie` +
+//             `&response_type=code`;
+//
+// // Open a new tab or window to the authorization URL
+//         window.open(authorizationUrl, '_blank');
+//     }
+// });
 
-        // Define the URL and parameters
-        const authorizationUrl = `https://ticktick.com/oauth/authorize?` +
-            `client_id=98tJj0kwfv0IvVmSFb` +
-            `&scope=tasks:write tasks:read` +
-            `&state=https://ticktick.com/oauth/authorize` +
-            `&redirect_uri=https://rezervacie.elfsport.sk/moje-rezervacie` +
-            `&response_type=code`;
+
+const authorizationUrl = `https://ticktick.com/oauth/authorize?` +
+    `client_id=98tJj0kwfv0IvVmSFb` +
+    `&scope=tasks:write tasks:read` +
+    `&state=https://ticktick.com/oauth/authorize` +
+    `&redirect_uri=https://rezervacie.elfsport.sk/moje-rezervacie` +
+    `&response_type=code`;
 
 // Open a new tab or window to the authorization URL
-        window.open(authorizationUrl, '_blank');
-    }
-});
-
+window.open(authorizationUrl, '_blank');
